3a72e5169a04670c744b4714ed115331e6d6cfb7
weightle55 <metalicboots@gmail.com>
2020-02-02 22:48:37 +0900

    컴포넌트와 prop(Properties) 사용하기

---
+++ chapter3/props/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MyComponent from './Mycomponent';
+
+const App = ()=>{
+  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+};
+
+export default App;
