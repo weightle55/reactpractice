925d3672d902168fefb5614f8873a7e8974cdbbf
weightle55 <metalicboots@gmail.com>
2020-02-03 19:05:41 +0900

    onChange를 하나의 메서드로 처리

--- chapter4/EventPracticeFunc.js
+++ chapter4/EventPracticeFunc.js
@@ -2,17 +2,28 @@
 import React,{useState} from 'react';
 
 const EventPractice = () =>{
-    const [username,setUsername]=useState('');
-    const [message,setMessage]=useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
-    const onClick = () => {
-        alert(username + ': '+message);
-        setUsername('');
-        setMessage('');
+    const [form,setForm] = useState({
+        username:'',
+        message: ''
+    });
+    const {username,message}=form;
+    const onChange = e =>{
+        const nextForm = {
+            ...form,
+            [e.target.name] : e.target.value
+        };
+        setForm(nextForm);
+    };
+
+    const onClick = () =>{
+        alert(username+': '+message);
+        setForm({
+            username: '',
+            message:''
+        });
     };
     const onKeyPress = e =>{
-        if(e.key ==='Enter'){
+        if(e.key==='Enter'){
             onClick();
         }
     };
@@ -24,12 +35,12 @@ const EventPractice = () =>{
             name = "username"
             placeholder="사용자명"
             value={username}
-            onChange={onChangeUsername} />
+            onChange={onChange} />
             <input
             type="text"
             name="message"
             placeholder="아무거나 입력해 보세요"
-            onChange={onChangeMessage} 
+            onChange={onChange} 
             onKeyPress={onKeyPress} 
             />
             <button onClick = {onClick}>확인</button>
